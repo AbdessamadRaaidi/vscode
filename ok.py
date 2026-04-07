70fe00e8958722f86458bda210d30666bd5c329d
AbdessamadRaaidi <abdessamad.raaidi@uit.ac.ma>
2026-04-07 09:18:32 +0100

    one code

---
+++ ok.py
@@ -0,0 +1,33 @@
+from cmath import rect
+
+import pygame
+
+
+pygame.Rect.inflate(x, y)
+pygame.Rect.inflate_ip(x, y)
+pygame.Rect.move(x, y)
+pygame.Rect.move_ip(x, y)
+pygame.Rect.topleft
+pygame.Rect.topright
+pygame.Rect.bottomleft
+pygame.Rect.bottomright
+pygame.Rect.center
+pygame.Rect.midtop
+pygame.Rect.midbottom
+pygame.Rect.midleft
+pygame.Rect.midright
+pygame.Rect.size
+pygame.Rect.width
+pygame.Rect.height
+pygame.Rect.left
+pygame.Rect.right
+pygame.Rect.top
+pygame.Rect.bottom
+pygame.Rect.collidepoint(x, y)
+pygame.Rect.colliderect(rect)
+pygame.Rect.collidelist(list)
+pygame.Rect.collidelistall(list)
+pygame.Rect.collidepoint(x, y)
+pygame.Rect.collidelist(list)
+pygame.Rect.collidelistall(list)
+pygame.Rect.colliderect(rect)
