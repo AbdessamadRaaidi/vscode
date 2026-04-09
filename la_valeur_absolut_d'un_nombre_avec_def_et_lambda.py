fb740bfd30818641985ee062d49f28d088f8275f
AbdessamadRaaidi <abdessamad.raaidi@uit.ac.ma>
2026-04-09 13:54:29 +0100

    on utilisant lambda et def

---
+++ la_valeur_absolut_d'un_nombre_avec_def_et_lambda.py
@@ -0,0 +1,9 @@
+def absolut(n):
+    if n<0:
+        return -n
+    else:
+        return n
+nombre=int(input("entrer un nombre:"))
+(lambda n: print(f"la valeur absolute de {n} est {(-n) if n<0 else n}"))(nombre)
+print(f"la valeur absolute de {nombre} est {absolut(nombre)}")
+
--- table_de_multiplication_with_lambda.py
+++ table_de_multiplication_with_lambda.py
@@ -1,3 +1,2 @@
-lambda n: [print(f"{n} x {i} = {n * i}") for i in range(1, 11)]
 nombre=int(input("entrer un nombre:"))
 (lambda n: [print(f"{n} x {i} = {n*i}") for i in range(1, 11)])(nombre)
