48ac4a32c3cc7145f0b9c09d1aab936ecbce96f8
AbdessamadRaaidi <abdessamad.raaidi@uit.ac.ma>
2026-04-13 14:29:19 +0100

    code

--- la_somme_des_factorielles.py
+++ la_somme_des_factorielles.py
@@ -8,8 +8,3 @@ s=0
 for i in range(n+1):
     s+=fact(i)
 print(f"La somme des factorielles de 0 à {n} est: {s}")
-t=0
-facto = lambda f, n: 1 if n == 0 else n * f(f, n - 1)
-for i in range(n+1):
-    t+=facto(facto, i)
-print(f"La somme des factorielles de 0 à {n} est: {t}")
